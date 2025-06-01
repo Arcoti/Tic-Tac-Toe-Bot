a12d2617a964441b9aa304b2b63589aef2e66d06
Arcoti <ArchdukeArcoti@gmail.com>
2025-06-01 12:51:47 +0800

    Added agent

---
+++ src/agent.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+import random
+from player import Player
+from board import Board
+
+# Q Learning Agent
+class Agent(Player):
+    def __init__(self, symbol: int, game: Board, alpha = 0.1, gamma = 0.9, epsilon = 0.1):
+        super().__init__(symbol, game)
+        self.Q = defaultdict(float)     # Dictionary with key as (state, action) and value as q value; Returns 0.0 when key is missing
+        self.alpha = alpha              # Learning Rate (Weight given to new information)
+        self.gamma = gamma              # Discount Factor (Importance of Future Reward)
+        self.epsilon = epsilon          # Exploration Probability
+    
+    # Get the q value 
+    def getQ(self, state, action):
+        return self.Q[(state, action)]
+    
+    # Let the agent perform an action
+    def chooseAction(self, state, actions):
+        if random.random() < self.epsilon:
+            # Explore actions
+            return random.choice(actions)
+        else:
+            # Choose the best actions
+            qValues = [self.getQ(state, action) for action in actions]
+            maxQ = max(qValues)
+            bestActions = [action for action, q in zip(actions, qValues) if q == maxQ]
+            return random.choice(bestActions)
+    
+    # Update q value base on future rewards
+    def update(self, state, action, reward, nextState, nextActions):
+        nextMaxQ = max([self.getQ(nextState, action) for action in nextActions], default = 0)
+        oldValue = self.Q[(state, action)]  
+        # Bellman Equation
+        newValue = oldValue + self.alpha * (reward + self.gamma * nextMaxQ - oldValue)
+        self.Q[(state, action)] = newValue
